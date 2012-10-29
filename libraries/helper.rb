cf05b03709089de045d536278c3c3b6987317cd7
Eric Saxby <sax@livinginthepast.org>
2012-10-29 10:33:34 -0700

    Fix typo in helper

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -2,7 +2,7 @@ module OhaiPrivateIpaddress
   module Helper
     def self.private_ip(host)
       addresses = host.network.interfaces.map do |name, interface|
-        interface['addresses'].keys if i['addresses']
+        interface['addresses'].keys if interface['addresses']
       end.flatten.compact
 
       addresses.grep(/^10\.|^172\.1[6-9]\.|^172\.2\d\.|^172\.3[0-1]\.|^192\.168/).first
