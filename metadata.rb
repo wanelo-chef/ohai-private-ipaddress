20577323b8a52298400c0fa6d58bf4f07bd56642
Eric Saxby <sax@livinginthepast.org>
2014-02-22 05:32:57 -0800

    Bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "ops@wanelo.com"
 license          "Apache 2.0"
 description      "Ohai plugin for getting a node's private IP address (RFC 1918)"
 
-version          "0.1.0"
+version          "0.1.1"
 
 depends          "ohai"
