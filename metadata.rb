dd742135deb05928932b2e1bc616118a64957a52
Eric Saxby <sax@livinginthepast.org>
2015-07-15 10:22:23 -0700

    0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "ops@wanelo.com"
 license          "Apache 2.0"
 description      "Ohai plugin for getting a node's private IP address (RFC 1918)"
 
-version          "0.1.1"
+version          "0.1.2"
 
 depends          "ohai"
